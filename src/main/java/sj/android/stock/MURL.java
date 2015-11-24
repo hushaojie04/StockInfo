6a50e076e53623b2f714cce0b33c7b3f94e0517b
hushaojie04 <645021167@qq.com>
2015-11-24 17:31:25 +0800

    服务器可用版

--- src/main/java/sj/android/stock/MURL.java
+++ src/main/java/sj/android/stock/MURL.java
@@ -6,8 +6,10 @@ import sj.utils.LogUtils;
  * Created by Administrator on 2015/10/31.
  */
 public class MURL {
-    public static final String HOST = "http://192.168.1.181";
-    private static final String BASE = "http://192.168.1.181:8080/JavaWebApp/action";
+//    public static final String HOST = "http://192.168.1.181";
+//    private static final String BASE = "http://192.168.1.181:8080/JavaWebApp/action";
+    public static final String HOST = "http://www.qushiw.com";
+    private static final String BASE = "http://www.qushiw.com:8080/WebServerForLOL/action";
     public static final String SERVER_URL = "http://www.qushiw.com";
     public static final  String YOUKU_PALYVIDEO = "http://player.youku.com/embed/XXXXXXXX==?";
 
